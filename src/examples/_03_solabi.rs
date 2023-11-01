06c9a7bf39510f989a800252cf2e91fd0bf36042
Ben Smith <bh2smith@gmail.com>
2023-11-01 16:34:22 +0100

    make dem getters

--- src/examples/_03_solabi.rs
+++ src/examples/_03_solabi.rs
@@ -1,44 +1,115 @@
 use ethrpc::{eth, types::*};
 use futures::future::join_all;
-use hex_literal::hex;
-use solabi::{
-    decode::{self},
-    Address,
-};
-use std::{borrow::Cow, error::Error};
+use solabi::{decode::Decode, encode::Encode, selector, Address, FunctionEncoder};
+use std::collections::HashMap;
 
-fn get_name_call(address: Address) -> TransactionCall {
+const NAME: FunctionEncoder<(), (String,)> = FunctionEncoder::new(selector!("name()"));
+const SYMBOL: FunctionEncoder<(), (String,)> = FunctionEncoder::new(selector!("symbol()"));
+const TOKEN_URI: FunctionEncoder<(U256,), (String,)> =
+    FunctionEncoder::new(selector!("tokenURI(uint256 tokenId)"));
+
+/// First (weak attempt)
+fn name_call(address: Address) -> TransactionCall {
     // Should be able to use solabi to construct `get_name_call` from function signature:
     // function name() public view virtual override returns (string memory)
     TransactionCall {
         to: Some(address),
-        input: Some(hex!("06fdde03").to_vec()),
+        // This is only the selector (since name() has no parameters)
+        input: Some(NAME.encode_params(&())),
         ..Default::default()
     }
 }
 
-pub async fn get_names(addresses: Vec<Address>) -> Result<(), Box<dyn Error>> {
+/// Using solabi docs example:
+fn symbol_call(address: Address) -> TransactionCall {
+    TransactionCall {
+        to: Some(address),
+        input: Some(SYMBOL.encode_params(&())),
+        ..Default::default()
+    }
+}
+
+fn uri_call(token: NftId) -> TransactionCall {
+    TransactionCall {
+        to: Some(token.address),
+        input: Some(TOKEN_URI.encode_params(&(token.id,))),
+        ..Default::default()
+    }
+}
+
+fn decode_function_result_string<T>(
+    res: &Result<Vec<u8>, ethrpc::http::Error>,
+    encoder: FunctionEncoder<T, (String,)>,
+) -> Option<String>
+where
+    T: Encode + Decode,
+{
+    match res {
+        Ok(bytes) => Some(encoder.decode_returns(bytes).expect("always blue").0),
+        Err(err) => {
+            tracing::warn!("got {:?}", err.to_string());
+            None
+        }
+    }
+}
+
+pub async fn get_name_and_symbol(
+    addresses: Vec<Address>,
+) -> HashMap<Address, (Option<String>, Option<String>)> {
     let client = ethrpc::http::Client::from_env().buffered(Default::default());
     tracing::info!("Preparing {} GetName Requests", addresses.len());
-    let futures = addresses
-        .into_iter()
-        .map(|addr| client.call(eth::Call, (get_name_call(addr), BlockId::default())));
+    let name_futures = addresses
+        .iter()
+        .cloned()
+        .map(|addr| client.call(eth::Call, (name_call(addr), BlockId::default())));
 
-    let x: Vec<_> = join_all(futures)
-        .await
+    let symbol_futures = addresses
+        .iter()
+        .cloned()
+        .map(|addr| client.call(eth::Call, (symbol_call(addr), BlockId::default())));
+
+    let names = join_all(name_futures).await;
+    let symbols = join_all(symbol_futures).await;
+
+    addresses
         .into_iter()
-        .map(|r| match r {
-            Ok(bytes) => {
-                // TODO use solabi::Decode::decode here!
-                // this seems like a hack.
-                decode::decode::<Cow<str>>(&bytes.as_slice()[32..])
-                    .unwrap()
-                    .to_string()
-            }
-            Err(err) => err.to_string(),
+        .zip(names.iter().zip(symbols))
+        .map(|(address, (name_result, symbol_result))| {
+            (
+                address,
+                (
+                    decode_function_result_string(name_result, NAME),
+                    decode_function_result_string(&symbol_result, SYMBOL),
+                ),
+            )
         })
-        .collect();
-    tracing::info!("Resolved {} GetName Requests", x.len());
-    println!("Names: {:?}", x);
-    Ok(())
+        .collect()
+}
+
+#[derive(Clone, PartialEq, Eq, Hash)]
+pub struct NftId {
+    pub address: Address,
+    pub id: U256,
+}
+
+pub async fn get_uris(token_ids: Vec<NftId>) -> HashMap<NftId, Option<String>> {
+    let client = ethrpc::http::Client::from_env().buffered(Default::default());
+    tracing::info!("Preparing {} tokenUri Requests", token_ids.len());
+    let name_futures = token_ids
+        .iter()
+        .cloned()
+        .map(|token| client.call(eth::Call, (uri_call(token), BlockId::default())));
+
+    let uris = join_all(name_futures).await;
+
+    token_ids
+        .into_iter()
+        .zip(uris)
+        .map(|(address, uri_result)| {
+            (
+                address,
+                decode_function_result_string(&uri_result, TOKEN_URI),
+            )
+        })
+        .collect()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod examples;
 mod util;
-use std::error::Error;
+use examples::_03_solabi::NftId;
+use solabi::Address;
+use std::{error::Error, str::FromStr};
 use tracing::Level;
 use tracing_subscriber::{filter::EnvFilter, FmtSubscriber};
 
@@ -13,7 +15,18 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let _ = examples::_01_repo_example::run_example().await;
     let _ = examples::_02_next_level::run_example(500).await;
-    let _ = examples::_03_solabi::get_names(util::addresses_from_file("./addresses.txt").unwrap())
-        .await;
+    let _ = examples::_03_solabi::get_name_and_symbol(
+        util::addresses_from_file("./addresses.txt").unwrap(),
+    )
+    .await;
+    let _ = examples::_03_solabi::get_uris(
+        (1u32..100)
+            .map(|i| NftId {
+                address: Address::from_str("0xD06966A7860131A8F858573BD76D08C27E7286BA").unwrap(),
+                id: i.into(),
+            })
+            .collect(),
+    )
+    .await;
     Ok(())
 }
